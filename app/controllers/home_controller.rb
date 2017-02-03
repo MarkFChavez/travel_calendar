37acc80fdb65cbbea78d3a8450b5e8d74606eaf5
Mark <markchav3z@gmail.com>
2017-02-03 14:29:48 +0800

    Only load events that should be displayed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
   def index
-    @events = Event.all
+    @events= Event.where(created_at: start_date.beginning_of_month..start_date.end_of_month)
+  end
+
+  private
+
+  def start_date
+    start_date = params[:start_date] || Date.today.to_s
+    Date.parse(start_date)
   end
 end
