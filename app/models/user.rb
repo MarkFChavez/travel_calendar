5aa54affc977cd2ca87e22dc519a8266303576b1
Mark <markchav3z@gmail.com>
2017-02-17 12:24:01 +0800

    add sign up temporarily

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable, :registerable
 
   has_many :events, dependent: :destroy
   has_many :notes, as: :resource
